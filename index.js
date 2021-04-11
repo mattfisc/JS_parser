2e193874bfc7153038992e74d412c455f96cfbfa
Matthew Fischer <mattfisc@gmail.com>
2021-04-10 21:25:54 -0700

    file stores all data to array of objects

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 data = [];
 
-const fs = require('fs')
+const fs = require('fs');
   
 fs.readFile('data.txt', (err, string_data) => {
     if (err) throw err;
@@ -9,6 +9,10 @@ fs.readFile('data.txt', (err, string_data) => {
     // Split data by Segment
     data_array = string_data.toString().split('||');
 
+    parse_data(data_array);
+})
+
+function parse_data(data_array){
     // Create Segment Object List
     for (let index = 0; index < data_array.length; index++) {
 
@@ -33,41 +37,77 @@ fs.readFile('data.txt', (err, string_data) => {
                 let value = element.substring(3);
                 // Check if fields are valid
                 if(name.match('[A-Z]{3}')){
-                    new_segment.add_field(name,value);
+                    var field = new Field(name,value);
+                    new_segment.add_field(field);
                 }
                 else{
                     console.log("error reading");
                 }
             }
-
-
         }
     }
-    
 
-    data.forEach(element => {
-        console.log(element.seg_name);
-        element.field.forEach(field => {
-            console.log(this.name);
-        });
-    });
-})
-
+    //tester
+    // data.forEach(element => {
+    //     console.log("Segment:\n\t",element.seg_name);
+    //     element.field.forEach(field => {
+    //         console.log("Field:\n\tname:",field.name);
+    //         console.log("\tvalue",field.value);
+    //     });
+    // });
+}
 
 function search(){
-    var input = document.getElementById('input');
-    var results = [];
+    var input = document.getElementById('input').value;
 
+    var result = [];
+    result.push( search_by_segment(input) );
+    result.push( search_by_name(input) );
+    result.push( search_by_value(input) );
 
 }
+
+function search_by_segment(input){
+    var result = [];
+
+    data.forEach(element => {
+        
+        if(element.localeCompare(input))
+            result.push(element);
+
+        element.field.forEach(field => {
+            if(field.name.localeCompare(input))
+                result.push(element);
+            if(field.value.localeCompare(input))
+            result.push(element);
+        });
+    });
+
+    return result;
+}
+
+// function search_by_name(input){
+//     var result = [];
+
+//     return result;
+// }
+// function search_by_value(input){
+//     var result = [];
+
+//     return result;
+// }
 
 
 
 class Segment{
-
+/**
+ * Segment Class
+ * @param seg_name String segments name to refer to the field list
+ * @param field Array String that lists all fields under the Segment
+ * 
+ */
 
     constructor(seg_name){
-
         // Segment
         this.seg_name = seg_name;
 
@@ -75,9 +115,8 @@ class Segment{
         this.field = [];
     }
 
-    add_field(name,value){
-        var add_field = new Field(name,value);
-        this.field.push(add_field);
+    add_field(field){
+        this.field.push(field);
     }
 
     get_seg_by_name(seg_name){
@@ -104,7 +143,11 @@ class Segment{
 
 
 class Field{
-
+/**
+ * Field Class
+ * @param name String a three upper case Letters related to value
+ * @param value String is the value field related to name
+ */
 
     constructor(name,value){
 
