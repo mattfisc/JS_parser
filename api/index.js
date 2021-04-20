7c2b99ae7c0a7ebb83452cc2b99b6463a32f9b5b
Matthew Fischer <mattfisc@gmail.com>
2021-04-20 13:39:33 -0700

    deleted unsed node.js module

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// ./api/index.js
-var Noder = require('noder.io').Noder;
-var api   = new Noder();
-// code body that constructs your API
-module.exports = api;
--- testcase.js
+++
@@ -1,6 +0,0 @@
-var api = require('./api');
-// load a plugin
-api.use('pluginName');
-// create an item in the container
-api.$di.set('someItem', 'value of the item');
-// ...
