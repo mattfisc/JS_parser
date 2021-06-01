a216c4c3e5b214fafd510db4b097117a0c71cb2a
Matthew Fischer <mattfisc@gmail.com>
2021-06-01 00:48:00 -0700

    Auto stash before merge of "main" and "origin/main"

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var file = "";
 var data = [];
 
@@ -185,4 +186,37 @@ class Field{
         return "name: " + this.name + " value: " + this.value;
     }
 }
+=======
+
+
+data = [];
+
+const fs = require('fs')
+  
+fs.readFile('data.txt', (err, string_data) => {
+    if (err) throw err;
+  
+    // Split data by Segment
+    data_array = string_data.toString().split('||');
+
+    // Create Segment Object List
+    for (let index = 0; index < data_array.length; index++) {
+        const element = data_array[index];
+        seg_array = element.split('|');
+
+        for (let index = 0; index < seg_array.length; index++) {
+            const element = seg_array[index];
+            
+            let re = '/[A-Z]*{3}/g'
+            // if segment 3 letters
+            if(element.match(re) && element.length == 3)
+                console.log(element);
+        }
+        
+        
+    }
+    //console.log(data.split('||'));
+    //console.log(data.toString());
+})
+>>>>>>> Stashed changes
 
